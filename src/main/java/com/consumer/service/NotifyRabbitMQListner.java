ddc2cdae56e76c23c3822e4138c66bf2248356a4
somgupta13 <somgupta13@gmail.com>
2020-06-12 23:34:04 +0530

    done final

--- src/main/java/com/consumer/service/NotifyRabbitMQListner.java
+++ src/main/java/com/consumer/service/NotifyRabbitMQListner.java
@@ -4,10 +4,14 @@ import com.consumer.Dao.NotificationDaoImpl;
 import com.consumer.Model.Notify;
 import com.consumer.Model.NotifyQueueRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.util.JSONPObject;
+//import jdk.nashorn.internal.parser.JSONParser;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 
 @Service
@@ -18,11 +22,17 @@ public class NotifyRabbitMQListner implements MessageListener {
     public void onMessage(Message message) {
         String body=message.toString();
         String arr[]=body.trim().split("\\s+");
-        String bodyattr=arr[0];
+         String bodyattr=arr[0];
         bodyattr=bodyattr.substring(7,bodyattr.length()-1);
-        NotifyQueueRequest notifyQueueRequest=new ObjectMapper().convertValue(bodyattr,NotifyQueueRequest.class);
 
-        System.out.println(notifyQueueRequest.getProductid());
+//        NotifyQueueRequest notifyQueueRequest= null;
+//        try {
+//            notifyQueueRequest = new ObjectMapper().readValue(bodyattr, NotifyQueueRequest.class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        System.out.println(bodyattr);
 //        System.out.println(message.getBody());
 //        NotifyQueueRequest notifyQueueRequest=new NotifyQueueRequest();
 //        Notify notify=notificationDaoImpl.insert(notifyQueueRequest);
